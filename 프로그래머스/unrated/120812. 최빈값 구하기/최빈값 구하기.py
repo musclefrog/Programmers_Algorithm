4a590458c26f9abef6fea7f67e62351530adbd52
Suin Kim <101573093+musclefrog@users.noreply.github.com>
2023-11-13 20:25:46 +0900

    [level unrated] Title: 최빈값 구하기, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120812. 최빈값 구하기/최빈값 구하기.py
@@ -0,0 +1,14 @@
+def solution(array):
+    answer = 0
+    set_array = set(array)
+    max_count = 0
+    
+    for i in set_array:
+        count = array.count(i)
+        if count > max_count:
+            max_count = count
+            answer = i
+        elif max_count == count:
+            answer = -1
+    
+    return answer

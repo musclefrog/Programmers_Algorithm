335fbd80da1f541f540dad17361a1ea8ca481cd5
Suin Kim <101573093+musclefrog@users.noreply.github.com>
2023-11-22 19:05:49 +0900

    [level unrated] Title: 수 조작하기 2, Time: 20.32 ms, Memory: 13.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.py
@@ -0,0 +1,12 @@
+def solution(numLog):
+    answer = []
+    for i in range(1, len(numLog)):
+        if numLog[i] - numLog[i-1] == 1:
+            answer.append('w')
+        elif numLog[i] - numLog[i-1] == -1:
+            answer.append('s')
+        elif numLog[i] - numLog[i-1] == 10:
+            answer.append('d')
+        elif numLog[i] - numLog[i-1] == -10:
+            answer.append('a')
+    return ''.join(answer)

b2742737189079a2c326de7f82048c8fcebd8124
Suin Kim <101573093+musclefrog@users.noreply.github.com>
2023-12-24 10:38:14 +0900

    [level unrated] Title: 왼쪽 오른쪽, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
@@ -0,0 +1,10 @@
+def solution(str_list):
+    answer = []
+    for i in range(len(str_list)):
+        if str_list[i] == 'l':
+            answer.append(str_list[:i])
+            break
+        elif str_list[i] == 'r':
+            answer.append(str_list[i+1:])
+            break
+    return (sum(answer, []))

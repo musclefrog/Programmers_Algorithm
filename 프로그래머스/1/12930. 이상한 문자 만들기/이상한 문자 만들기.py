9d0366a981ce96d44190a6b4109dce25d8ec6437
Suin Kim <101573093+musclefrog@users.noreply.github.com>
2024-04-09 22:06:33 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -0,0 +1,11 @@
+def solution(s):
+    answer = ''
+    s_list = s.split(' ')
+    for word in s_list:
+        for i in range(len(word)):
+            if i % 2 == 0:
+                answer += word[i].upper()
+            else:
+                answer += word[i].lower()
+        answer += ' '
+    return answer[0:-1]

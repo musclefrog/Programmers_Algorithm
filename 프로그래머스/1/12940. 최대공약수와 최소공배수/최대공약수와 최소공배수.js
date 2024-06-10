73415dca970aa8df94c4f0f68d64d3e5da1d2e4a
Suin Kim <101573093+musclefrog@users.noreply.github.com>
2024-06-10 22:05:15 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 2.56 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -0,0 +1,17 @@
+function cal(n, m) {
+    let gcd = 0;
+    let i = 1;
+    while(i <= Math.min(n, m)) {
+        if(n % i === 0 && m % i === 0) {
+            gcd = i;
+            console.log(gcd);
+        }
+        i++;
+    }
+    let lcm = gcd * (n / gcd) * (m / gcd);
+    return [gcd, lcm];
+}
+
+function solution(n, m) {
+    return cal(n, m);
+}

2f8fb7cad9ac117d7e04fc34505f1aeb0c5cec28
Suin Kim <101573093+musclefrog@users.noreply.github.com>
2024-06-10 22:02:34 +0900

    [level 1] Title: 2016년, Time: 3.22 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -0,0 +1,8 @@
+function solution(a, b) {
+    const months = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    const days = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"];
+    let arr = months.slice(0, a);
+    console.log(arr);
+    let day = (arr.reduce((x,y) => x+y, 0) + b) % 7;
+    return days[day];
+}

8993eb9f40e917d9e560cb01e312c0dd3dd3160b
Suin Kim <101573093+musclefrog@users.noreply.github.com>
2024-06-21 18:41:55 +0900

    [level 1] Title: 기사단원의 무기, Time: 3.93 ms, Memory: 37.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -0,0 +1,24 @@
+function solution(number, limit, power) {
+    let divCnt = [];
+    let i = 1;
+    
+    // 약수 배열 생성
+    for(let num=1; num<=number; num++) {
+        let i = 1;
+        let divArr = [];
+        while(i <= Math.sqrt(num)) {
+            if(num % i === 0) {
+                divArr.push(i);
+                if(num / i !== i) {
+                    divArr.push(num / i);
+                }
+            }
+            i++;
+        }
+        divCnt.push(divArr.length);
+    }
+    
+    // limit을 초과하는 기사는 power로 바꾸어서 더하기
+    let arr = divCnt.map((x) => x > limit ? power : x);
+    return arr.reduce((a,b) => a+b, 0);
+}

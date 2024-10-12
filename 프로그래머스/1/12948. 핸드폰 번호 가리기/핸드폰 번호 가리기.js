e5933b27d0dadc7b81c1c57bad15eca74d3d232a
Suin Kim <101573093+musclefrog@users.noreply.github.com>
2024-10-12 22:23:08 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,3 +1,4 @@
 function solution(phone_number) {
-    return '*'.repeat(phone_number.length-4) + phone_number.slice(phone_number.length-4, phone_number.length);
+    const len = phone_number.length;
+    return '*'.repeat(len-4) + phone_number.slice(len-4, len);
 }

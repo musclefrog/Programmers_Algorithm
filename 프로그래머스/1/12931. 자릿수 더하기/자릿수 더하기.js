f996c4521f3b51c1e2d8f32a91f0a618d962d422
Suin Kim <101573093+musclefrog@users.noreply.github.com>
2024-10-12 15:54:21 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,5 +1,8 @@
-function solution(n)
-{
-    const arr = String(n).split("")
-    return arr.reduce((a,b) => Number(a) + Number(b), 0);
+function solution(n) {
+    let answer = 0;
+    while(n > 0) {
+        answer += n % 10;
+        n = Math.floor(n / 10);
+    }
+    return answer;
 }

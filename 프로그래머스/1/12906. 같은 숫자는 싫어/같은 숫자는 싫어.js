1d995dcff07de821119b203da3a974003ea0c87d
Suin Kim <101573093+musclefrog@users.noreply.github.com>
2024-10-13 17:03:31 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 32.25 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,13 +1,10 @@
-function solution(arr)
-{
+function solution(arr) {
     let stack = [];
-    for(let i=0; i<arr.length; i++) {
+    for(let i = 0; i < arr.length; i++) {
         if(arr[i] !== stack[stack.length-1]){
             stack.push(arr[i]);    
         }
-        else {
-            continue;
-        }
+        else continue;
     }
     return stack;
 }

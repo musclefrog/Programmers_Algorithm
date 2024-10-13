69e02e13d2bbffa7c2c4de284b22c77c1f14df6f
Suin Kim <101573093+musclefrog@users.noreply.github.com>
2024-10-13 16:36:00 +0900

    [level 1] Title: 직사각형 별찍기, Time: 44.19 ms, Memory: 32 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -2,8 +2,7 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
     const n = data.split(" ");
     const a = Number(n[0]), b = Number(n[1]);
-    let row = '*'.repeat(a);
-    for(let i=0; i<b; i++){
-        console.log(row);
+    for(let i = 0; i < b; i++) {
+        console.log('*'.repeat(a));
     }
 });

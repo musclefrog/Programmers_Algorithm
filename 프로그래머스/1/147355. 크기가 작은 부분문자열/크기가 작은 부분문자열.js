1d057c6065d6e00d42325cad034c0025dde4f15f
Suin Kim <101573093+musclefrog@users.noreply.github.com>
2024-10-13 17:58:52 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 4.50 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,7 +1,7 @@
 function solution(t, p) {
-    let cnt = 0;
-    for(let i=0; i<t.length-p.length+1; i++){
-        cnt += +t.slice(i, i + p.length) <= p ? 1 : 0;
+    let answer = 0;
+    for(let i = 0; i <= t.length - p.length; i++) {
+        answer += +t.slice(i, i + p.length) <= p ? 1 : 0;
     }
-    return cnt;
+    return answer;
 }

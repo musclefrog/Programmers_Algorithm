b34d09a53adea7264dd27b26e554892d2bff2bdb
Suin Kim <101573093+musclefrog@users.noreply.github.com>
2024-10-13 16:17:46 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,16 +1,13 @@
 function solution(s) {
     let answer = true;
     const arr = s.split('');
-    
-    if(arr.length === 4 || arr.length === 6){
-        for(let i=0; i<arr.length; i++){
-            if(isNaN(arr[i])){
+    if (s.length === 4 || s.length === 6) {
+        for (let i=0; i<arr.length; i++) {
+            if(isNaN(arr[i])) {
                 answer = false;
                 break;
             }
         }
-    } else {
-        answer = false;
-    }
+    } else { answer = false; }
     return answer;
 }

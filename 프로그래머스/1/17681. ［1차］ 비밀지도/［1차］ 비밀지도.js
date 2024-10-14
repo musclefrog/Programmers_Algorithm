997bd26ad03052d0de0134529f51c674ab210373
Suin Kim <101573093+musclefrog@users.noreply.github.com>
2024-10-14 17:50:57 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.38 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,15 +1,15 @@
 function solution(n, arr1, arr2) {
     let answer = [];
     // '#'로 채워진 2차원 배열 생성. 지도 1과 지도 2를 합친 결과를 표현.
-    const newArr = Array.from(Array(n), () => Array(n).fill('#'))
+    const newArr = Array.from(Array(n), () => Array(n).fill('#'));
     
     // 지도를 2진수로 변환하는 함수.
     function binary(arr) {
         let binArr = [];
-        for(let i=0; i<n; i++) {
-            let binNum = parseInt(arr[i].toString(2)).toString();
+        for(let i = 0; i < n; i++) {
+            let binNum = arr[i].toString(2);
             binArr.push(binNum.split(""));
-            for(let j=0; j<n-binNum.length; j++) {
+            for(let j = 0; j < n - binNum.length; j++) {
                 binArr[i].unshift('0');
             }
         }
@@ -21,13 +21,14 @@ function solution(n, arr1, arr2) {
     binArr2 = binary(arr2);
     
     // 지도 1과 지도 2를 합친 지도 생성.
-    for(let i=0; i<n; i++) {
-        for(let j=0; j<n; j++) {
+    for(let i = 0; i < n; i++) {
+        for(let j = 0; j < n; j++) {
             if(binArr1[i][j] === '0' && binArr2[i][j] === '0') {
                 newArr[i][j] = ' ';
             }
         }
     }
+    
     // 새로운 지도의 2차원 배열을 1차원 문자열 배열로 만들어줌.
     for(let s of newArr) {
         let k = s.join('');

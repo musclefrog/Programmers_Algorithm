d154297dafd09b9d01fd7d7ce44c66621f572289
Suin Kim <101573093+musclefrog@users.noreply.github.com>
2024-10-14 16:50:14 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 39.60 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,12 +1,7 @@
 function solution(s) {
     let answer = [];
-    
     for(let i = 0; i < s.length; i++) {
-        if(s.slice(0, i).includes(s[i])) {
-            answer.push(i - s.slice(0, i).lastIndexOf(s[i]));
-        } else {
-            answer.push(-1);
-        }
+        answer.push(s.slice(0, i).includes(s[i]) ? i - s.slice(0, i).lastIndexOf(s[i]) : -1);
     }
     return answer;
 }

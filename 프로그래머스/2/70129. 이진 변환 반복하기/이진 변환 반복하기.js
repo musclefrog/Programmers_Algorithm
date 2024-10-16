5d3bc3f73b31f02bc80f2139730e1fad9d4675b3
Suin Kim <101573093+musclefrog@users.noreply.github.com>
2024-10-16 23:14:03 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 12.77 ms, Memory: 41.1 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,18 +1,9 @@
-function binary(num) {
-    let arr = [];
-    while(num !== 0) {
-        arr.push(num % 2);
-        num = Math.floor(num / 2);
-    }
-    return arr.reverse().join('');
-}
-
 function solution(s) {
     let answer = [];
     while(s !== '1') {
         let cntZero = s.split('').filter((el) => el === '0').length;
         let cntOne = s.length - cntZero;
-        s = binary(cntOne);
+        s = cntOne.toString(2);
         answer.push(cntZero);
     }
     return [answer.length, answer.reduce((a,b) => a+b, 0)];

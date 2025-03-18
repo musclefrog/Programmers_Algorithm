6456374c82054a61fead82469c54be60bdaa195b
Suin Kim <101573093+musclefrog@users.noreply.github.com>
2025-03-18 13:45:39 +0900

    [level 2] Title: 주식가격, Time: 74.26 ms, Memory: 18.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -8,9 +8,9 @@ def solution(prices):
         cnt = 0
         x = queue.popleft()
         for i in queue:
-            if x <= i:
+            if x <= i: # 뒤에 남은 숫자가 같거나 작으면 cnt += 1
                 cnt += 1
-            else:
+            else: # 뒤에 남은 숫자가 같거나 작지 않을 경우 cnt += 1 하고 break
                 cnt += 1
                 break
         answer.append(cnt)

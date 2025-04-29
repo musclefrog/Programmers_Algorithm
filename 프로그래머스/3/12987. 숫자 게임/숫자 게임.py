4eb97acbda1bcdcfc7fa3b4ae70afbc97404b8f8
Suin Kim <101573093+musclefrog@users.noreply.github.com>
2025-04-29 16:17:57 +0900

    [level 3] Title: 숫자 게임, Time: 2670.78 ms, Memory: 18.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+def solution(A, B):
+    answer = 0
+    A = sorted(A, reverse=True)
+    B = sorted(B, reverse=True)
+    
+    p = 0
+    
+    for i in range(len(A)):
+        for j in range(p, len(B)):
+            if A[i] < B[j]:
+                answer += 1
+                p = j + 1
+                break
+                
+    return answer

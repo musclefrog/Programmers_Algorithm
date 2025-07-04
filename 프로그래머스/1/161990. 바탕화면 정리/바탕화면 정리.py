e9ae7235afe35ff35e43573b170c7d6f85cd6720
Suin Kim <101573093+musclefrog@users.noreply.github.com>
2025-07-04 12:15:49 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.34 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,25 +1,11 @@
 def solution(wallpaper):
-    # 파일 좌표 저장
-    file = []
+    x = []
+    y = []
+    
     for i in range(len(wallpaper)):
         for j in range(len(wallpaper[0])):
             if wallpaper[i][j] == '#':
-                file.append([i, j])
+                y.append(i)
+                x.append(j)
     
-    # 위/아래/왼/오 끝 점 구하기
-    max_y = file[0][0]
-    min_y = file[0][0]
-    max_x = file[0][1]
-    min_x = file[0][1]
-    
-    for co in file:
-        if co[0] > max_y:
-            max_y = co[0]
-        if co[0] < min_y:
-            min_y = co[0]
-        if co[1] > max_x:
-            max_x = co[1]
-        if co[1] < min_x:
-            min_x = co[1]
-    
-    return [min_y, min_x, max_y + 1, max_x + 1]
+    return [min(y), min(x), max(y) + 1, max(x) + 1]

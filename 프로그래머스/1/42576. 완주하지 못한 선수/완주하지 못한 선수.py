210d815e60b03126460938e026ec602d40b16922
Suin Kim <101573093+musclefrog@users.noreply.github.com>
2025-08-08 16:55:59 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 49.49 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,15 +1,15 @@
 def solution(participant, completion):
-    hashDict = {}
-    sumHash = 0
-    
-    # 해시 딕셔너리 생성
-    for part in participant:
-        hashDict[hash(part)] = part
-        sumHash += hash(part)
-        
-    # 해시의 합에서 완주자 해시를 빼면 미완주자의 해시만 남음
-    for comp in completion:
-        sumHash -= hash(comp)
-    
-    # 미완주자 해시의 key 값이 미완주자의 이름임
-    return hashDict[sumHash]
+    dic = {}
+
+    # 참가자 이름 등장 횟수 기록
+    for p in participant:
+        dic[p] = dic.get(p, 0) + 1
+
+    # 완주자 이름 등장 횟수 차감
+    for c in completion:
+        dic[c] -= 1
+
+    # 값이 1 이상인 key가 완주 못한 사람
+    for key, value in dic.items():
+        if value > 0:
+            return key

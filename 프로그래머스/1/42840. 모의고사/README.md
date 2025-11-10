2f21461998aeffa3e1cdf0fc1e5d724539c1efa5
Suin Kim <101573093+musclefrog@users.noreply.github.com>
2025-11-10 21:24:28 +0900

    [level 1] Title: 모의고사, Time: 6.29 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 9.28 MB, 시간: 8.22 ms
+메모리: 9.18 MB, 시간: 6.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 05일 17:10:31
+2025년 11월 10일 21:24:23
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,20 +1,20 @@
 def solution(answers):
-    answer = []
+    student1 = [1, 2, 3, 4, 5]
+    student2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    student3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    score = [0, 0, 0]
     result = []
     
-    test = [[1, 2, 3, 4, 5],
-            [2, 1, 2, 3, 2, 4, 2, 5],
-            [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
-
-    for t in test:
-        cnt = 0
-        for i in range(len(answers)):
-            if t[i % len(t)] == answers[i]:
-                cnt += 1
-        result.append(cnt)
+    for idx, answer in enumerate(answers):
+        if answer == student1[idx % len(student1)]:
+            score[0] += 1
+        if answer == student2[idx % len(student2)]:
+            score[1] += 1
+        if answer == student3[idx % len(student3)]:
+            score[2] += 1
         
-    for i in range(3):
-        if result[i] == max(result):
-            answer.append(i+1)
+    for idx, s in enumerate(score):
+        if s == max(score):
+            result.append(idx + 1)
     
-    return answer
+    return result
